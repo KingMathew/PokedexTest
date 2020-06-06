0f24bb1c01cd430ae2be597ffa254cb6fb2eda6d
Mateo Ortiz <mortiz@proximateapps.com>
2020-06-06 15:45:05 -0500

    Initial Commit

---
+++ PokedexTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokedexTest
+//
+//  Created by Proximate on 6/06/20.
+//  Copyright © 2020 mateo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
